9086a1cd14c8505a548b6e0dbb30d5d423106376
Daeil Bae <dlbae9613@gmail.com>
2022-10-26 17:57:32 +0900

    생산진행정보입력 조회 기능 구현 (테스트 준비)

--- scm_front/js/production_progress_information.js
+++ scm_front/js/production_progress_information.js
@@ -5,20 +5,23 @@ let app = new Vue({
         deptName: GX.Cookie.get('DeptName'),
         userName: GX.Cookie.get('UserName'),
         params: GX.getParameters(),
+        BizUnitList: [], // 사업 단위 리스트
+        isCheckList: [],
 
         rows: {
             Query: [],
         },
         queryForm:{
             CompanySeq: GX.Cookie.get('CompanySeq'),
-            BizUnit: '',
+            BizUnit: '1',
             WorkDateFr: new Date().toLocaleDateString().replace(/\./g, "").replace(/\ /g, "-"),
             WorkDateTo: new Date().toLocaleDateString().replace(/\./g, "").replace(/\ /g, "-"),
             WorkOrderDateFr: '',
             WorkOrderDateTo: '',
             GoodItemName: '',
             GoodItemNo: '',
-            GoodItemSpec: ''
+            GoodItemSpec: '',
+            CustSeq: ''
         },
         keyCombi: {
             isKeyHold: false,
@@ -40,7 +43,7 @@ let app = new Vue({
             }
 
             // Key Event
-            if(e.type === 'keyup'){
+            else if(e.type === 'keyup'){
                 switch(e.key.toLowerCase()){
                     case 'control': vThis.keyCombi.Control = false; break;
                     case 'q': vThis.keyCombi.Q = false; break;
@@ -101,7 +104,30 @@ let app = new Vue({
             }
         },
 
-        // 조회
+        // 초기화
+        init: function(){
+            let vThis = this;
+            vThis.initKeyCombi();
+            vThis.rows.Query = [];
+            vThis.queryForm.CompanySeq = GX.Cookie.get('CompanySeq');
+            vThis.queryForm.BizUnit = '1';
+            vThis.queryForm.WorkDateFr = new Date().toLocaleDateString().replace(/\./g, "").replace(/\ /g, "-");
+            vThis.queryForm.WorkDateTo = new Date().toLocaleDateString().replace(/\./g, "").replace(/\ /g, "-");
+            vThis.queryForm.WorkOrderDateFr = '';
+            vThis.queryForm.WorkOrderDateTo = '';
+            vThis.queryForm.GoodItemName = '';
+            vThis.queryForm.GoodItemNo = '';
+            vThis.queryForm.GoodItemSpec = '';
+            vThis.queryForm.CustSeq = '';
+        },
+
+        initKeyCombi: function(){
+            Object.keys(this.keyCombi).map(k => {
+                this.keyCombi[k] = false;
+            });
+        },
+
+        /** 조회 **/
         search: function(){
             let vThis = this;
 
@@ -117,14 +143,21 @@ let app = new Vue({
                 .setMethodId('')    // 여기에 호출ID를 입력해주세요.
                 .ajax([params], [function (data){
                     if(data.length > 0){
+
+                        // 조회 결과를 가져와서 그리드에 출력한다.
                         for(let i in data){
-                            if(data.hasOwnProperty(i))
+                            if(data.hasOwnProperty(i)){
                                 data[i].ROWNUM = parseInt(i) + 1;
+                                data[i].RowEdit = false;
+                                data[i].WorkOrderDate = data[i].WorkOrderDate.length == 8 ? (data[i].WorkOrderDate.replace(/(\d{4})(\d{2})(\d{2})/, '$1-$2-$3')) : data[i].WorkOrderDate;
+                                data[i].WorkDate = data[i].WorkDate.length == 8 ? (data[i].WorkDate.replace(/(\d{4})(\d{2})(\d{2})/, '$1-$2-$3')) : data[i].WorkDate;
+
+                            }
                         }
-                        console.log(data);
                         vThis.rows.Query = data;
 
                     } else{
+                        vThis.rows.Query = [];
                         alert('조회 결과가 없습니다.');
                     }
                 }])
@@ -147,6 +180,14 @@ let app = new Vue({
             document.addEventListener('keydown', vThis.eventCheck, false);
             document.addEventListener('keyup', vThis.eventCheck, false);
 
+            // 사업단위가 여러개일 수 있기에 아래와 같이 set/get함
+            let objBizUnitList = JSON.parse(GX.Cookie.get('BizUnit_JsonFormatStringType'));
+            Object.keys(objBizUnitList).map((k) => {
+                vThis.BizUnitList.push(objBizUnitList[k]);
+            });
+            vThis.queryForm.CompanySeq = vThis.BizUnitList[0].CompanySeq;
+            vThis.queryForm.BizUnit = vThis.BizUnitList[0].BizUnit;
+            vThis.queryForm.BizUnitName = vThis.BizUnitList[0].BizUnitName;
 
             GX.VueGrid
                 .bodyRow(':class="{\'check\':isChecked(index)}"')
@@ -154,9 +195,9 @@ let app = new Vue({
                 .item('WorkOrderDate').head('작업지시일', '')
                 .item('WorkDate').head('작업예정일', '')
                 .item('WorkOrderNo').head('작업지시번호', '')
-                .item('ItemName').head('품명', '')
-                .item('ItemNo').head('품번', '')
-                .item('Spec').head('규격', '')
+                .item('ItemName').head('품명', '').body(null, 'text-l')
+                .item('ItemNo').head('품번', '').body(null, 'text-l')
+                .item('Spec').head('규격', '').body(null, 'text-l')
                 .item('CutInPutDate').head('재단최초투입일', '') // Input
                 .item('CutQty').head('재단량', '') // Input
                 .item('SewInPutDate').head('봉제최초투입일', '') // Input
@@ -174,12 +215,19 @@ let app = new Vue({
             height: '400px',
             monthSelectWidth: '25%',
             callback: function(result, attribute){
-                const openerObj = document.querySelector('[name="' + GX.Calendar.openerName + '"]');
-                const info = GX.Calendar.dateFormatInfo(openerObj);
-                let keys = attribute.split('.');
-                if (keys.length == 1 && vThis[keys[0]] != null) vThis[keys[0]] = (result.length == 0) ? '' : GX.formatDate(result, info.format);
-                else if (keys.length == 2 && vThis[keys[0]][keys[1]] != null) vThis[keys[0]][keys[1]] = (result.length == 0) ? '' : GX.formatDate(result, info.format);
-                else if (keys.length == 3 && vThis[keys[0]][keys[1]][keys[2]] != null) vThis[keys[0]][keys[1]][keys[2]] = (result.length == 0) ? '' : GX.formatDate(result, info.format);
+                if(!isNaN(attribute)){
+                    vThis.rows.Query[attribute][GX.Calendar.openerName] = result;
+                    vThis.rows.Query[attribute].RowEdit = true;
+                    document.getElementsByName(GX.Calendar.openerName)[attribute].parentNode.parentNode.classList.add('no-data');
+                } else{
+                    const openerObj = document.querySelector('[name="' + GX.Calendar.openerName + '"]');
+                    const info = GX.Calendar.dateFormatInfo(openerObj);
+                    let keys = attribute.split('.');
+                    if (keys.length == 1 && vThis[keys[0]] != null) vThis[keys[0]] = (result.length == 0) ? '' : GX.formatDate(result, info.format);
+                    else if (keys.length == 2 && vThis[keys[0]][keys[1]] != null) vThis[keys[0]][keys[1]] = (result.length == 0) ? '' : GX.formatDate(result, info.format);
+                    else if (keys.length == 3 && vThis[keys[0]][keys[1]][keys[2]] != null) vThis[keys[0]][keys[1]][keys[2]] = (result.length == 0) ? '' : GX.formatDate(result, info.format);
+                    vThis.updateDate(GX.formatDate(result, info.format, openerObj));
+                }
             }
         });
     }
