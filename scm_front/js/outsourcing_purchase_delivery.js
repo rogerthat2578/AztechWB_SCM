5585dbe63ef1c98affb648d921be8362a7e0d49d
Daeil Bae <dlbae9613@gmail.com>
2022-10-31 13:04:26 +0900

    외주납품입력 저장, 행 삭제, 삭제 기능 구현

--- scm_front/js/outsourcing_purchase_delivery.js
+++ scm_front/js/outsourcing_purchase_delivery.js
@@ -88,6 +88,20 @@ let app = new Vue({
             }
             this.isCheckList = isCheckList;
         },
+        selectedAllIsEnd: function(){
+            let obj = document.querySelectorAll('[name="IsEnd"]');
+            console.log(obj);
+            for(let i in obj){
+                if(obj.hasOwnProperty(i)){
+                    obj[i].checked = event.target.checked;
+                    if(event.target.checked)
+                        this.rows.Query[i].IsEnd = 1;
+                    else
+                        this.rows.Query[i].IsEnd = 0;
+                }
+            }
+            console.log(this.rows.Query);
+        },
         initSelected: function(){
             this.isCheckList = [];
             let setAllObj = document.querySelector('thead [type="checkbox"]');
@@ -103,6 +117,11 @@ let app = new Vue({
             let idx = this.isCheckList.indexOf(index);
             if(event.target.checked) this.isCheckList.push(index);
             else if(idx != -1) this.isCheckList.splice(idx, 1);
+        },
+        selectedMarkIsEnd: function(index){
+            if(event.target.checked) this.rows.Query[index].IsEnd = 1;
+            else this.rows.Query[index].IsEnd = 0;
+            console.log(this.rows.Query);
         },
         applyAll: function(name, idx){
             event.target.setAttribute('gx-datepicker', idx);
@@ -126,7 +145,6 @@ let app = new Vue({
                 vThis.summaryArea[k] = 0;
             });
 
-            console.log('QueryLen : ' + vThis.rows.Query.length);
             if(vThis.rows.Query.length > 0){
                 let calList = GX.deepCopy(vThis.rows.Query);
                 for(let i in calList){
@@ -138,6 +156,10 @@ let app = new Vue({
                         });
                     }
                 }
+            }
+
+            for(let sumName in vThis.summaryArea){
+                vThis.summaryArea[sumName] = vThis.summaryArea[sumName].toString().replace(/(\d)(?=(?:\d{3})+(?!\d))/g, '$1,');
             }
         },
 
@@ -215,17 +237,24 @@ let app = new Vue({
 
         save: function(){
             let vThis = this;
-            console.log(vThis.rows.Query);
-            alert('준비중입니다.');
-
-            /*
-
-            let params1 = [], params2 = [];
             let saveArrData = GX.deepCopy(vThis.rows.Query);
 
             for(let i = saveArrData.length - 1; i >= 0; i--){
                 if(saveArrData[i].RowEdit){
                     saveArrData[i].IDX_NO = saveArrData[i].ROWNUM;
+                    saveArrData[i].OrderQty = parseFloat(saveArrData[i].OrderQty.toString().replace(/\,/g, ''));
+                    saveArrData[i].ProgressQty = parseFloat(saveArrData[i].ProgressQty.toString().replace(/\,/g, ''));
+                    saveArrData[i].ProdQty = parseFloat(saveArrData[i].ProdQty.toString().replace(/\,/g, ''));
+                    saveArrData[i].OKQty = parseFloat(saveArrData[i].OKQty.toString().replace(/\,/g, ''));
+                    saveArrData[i].BadQty = parseFloat(saveArrData[i].BadQty.toString().replace(/\,/g, ''));
+                    saveArrData[i].OSPPrice = parseFloat(saveArrData[i].OSPPrice.toString().replace(/\,/g, ''));
+                    saveArrData[i].OSPCurAmt = parseFloat(saveArrData[i].OSPCurAmt.toString().replace(/\,/g, ''));
+                    saveArrData[i].OSPCurVAT = parseFloat(saveArrData[i].OSPCurVAT.toString().replace(/\,/g, ''));
+                    saveArrData[i].OSPTotCurAmt = parseFloat(saveArrData[i].OSPTotCurAmt.toString().replace(/\,/g, ''));
+                    saveArrData[i].OSPDomPrice = parseFloat(saveArrData[i].OSPDomPrice.toString().replace(/\,/g, ''));
+                    saveArrData[i].OSPDomVAT = parseFloat(saveArrData[i].OSPDomVAT.toString().replace(/\,/g, ''));
+                    saveArrData[i].OSPDomAmt = parseFloat(saveArrData[i].OSPDomAmt.toString().replace(/\,/g, ''));
+                    saveArrData[i].OSPTotDomAmt = parseFloat(saveArrData[i].OSPTotDomAmt.toString().replace(/\,/g, ''));
 
                     // 외주납품품목조회 Jump
                     if(vThis.jumpSetMethodId == 'PDWorkReportJumpQuery'){
@@ -241,23 +270,10 @@ let app = new Vue({
                 }
             }
 
-            // Master
-            params1 = [vThis.queryForm];
-            params1[0].IDX_NO = saveArrData[0].ROWNUM;
-            params1[0].UserId = GX.Cookie.get('UserId');
-            params1[0].DelvDate = params1[0].DelvDate.indexOf('-') > -1 ? params1[0].DelvDate.replace(/\-/g, "") : params1[0].DelvDate;
-            if(vThis.jumpSetMethodId == 'PDWorkReportJumpQuery'){
-                params1[0].WorkingTag = 'U';
-            } else if(vThis.jumpSetMethodId == 'OSPWorkOrderJump'){
-                params1[0].WorkingTag = 'A';
-            }
-
-            // Detail
-            params2 = saveArrData;
-            if(params1.length > 0 && params2.length > 0){
+            if(saveArrData.length > 0){
                 GX._METHODS_
-                .setMethodId('')    // 여기에 API 키 입력
-                .ajax(params1, params2, [function (data) {
+                .setMethodId('PDWorkReportSave')    // 여기에 API 키 입력
+                .ajax(saveArrData, [], [function (data) {
                     if(data[0].Status && data[0].Status != 0){
                         alert('저장 실패\n' + data[0].Result);
 
@@ -269,20 +285,15 @@ let app = new Vue({
                         }
                         alert('저장 성공');
                     }
-                }, function(data){
-
                 }]);
+
             } else{
                 alert('저장할 데이터가 없습니다.');
             }
-            */
         },
 
         delRow: function(){
             let vThis = this;
-            alert('준비중입니다.');
-
-            /*
 
             if(vThis.rows.Query.length < 1 || vThis.isCheckList.length == 0) {
                 alert('삭제할 데이터가 없습니다. 삭제할 데이터를 선택 후 삭제해주세요.');
@@ -294,35 +305,26 @@ let app = new Vue({
                     // 외주발주품목조회에서 넘어온 데이터 전체 삭제 시 뒤로가기
                     if(vThis.jumpSetMethodId == 'OSPWorkOrderJump'){
                         history.back(-1);
-
                     }
                     // 외주납품품목조회에서 넘어온 데이터 전체 삭제 시
                     else if(vThis.jumpSetMethodId == 'PDWorkReportJumpQuery'){
                         let delArrData = GX.deepCopy(vThis.rows.Query);
 
-                        let params1 = [], params2 = [];
                         for(let i in delArrData){
-                            let objParams = {};
                             if(delArrData.hasOwnProperty(i)){
-                                objParams.DelvSeq = delArrData[i].DelvSeq;      // ?
-                                objParams.DelvSerl = delArrData[i].DelvSerl;    // ?
-                                objParams.WorkingTag = 'D';
-                                params2.push(objParams);
+                                delArrData[i].WorkingTag = 'D';
                             }
                         }
 
-                        params1.push(params2[0]);
-                        if(params1.length > 0 && params2.length > 0){
+                        if(delArrData.length > 0){
                             GX._METHODS_
-                            .setMethodId('')    // 여기에 API 키 입력
-                            .ajax(params1, params2, [function (data){
+                            .setMethodId('PDWorkReportSave')    // 여기에 API 키 입력
+                            .ajax(delArrData, [], [function (data){
                                 if(data[0].Status && data[0].Status != 0){
                                     alert('삭제 실패\n' + data[0].Result);
                                 } else{
                                     alert('삭제 성공');
                                 }
-                            }, function(data) {
-
                             }]);
                         } else{
                             alert('삭제할 데이터가 없습니다.');
@@ -355,23 +357,15 @@ let app = new Vue({
                 // 행 삭제 (외주납품에서 넘어온 데이터인 경우)
                 else if(vThis.jumpSetMethodId == 'PDWorkReportJumpQuery'){
                     let delArrData = GX.deepCopy(vThis.rows.Query);
-                    let params1 = [], params2 = [];
                     for(let i in delArrData){
-                        let objParams = {};
                         if(delArrData.hasOwnProperty(i)){
-                            objParams.DelvSeq = delArrData[i].DelvSeq;      // ?
-                            objParams.DelvSerl = delArrData[i].DelvSerl;    // ?
-                            objParams.WorkingTag = 'D';
-                            params2.push(objParams);
+                            delArrData[i].WorkingTag = 'D';
                         }
                     }
 
-                    params1.push(params2[0]);
-
                     GX._METHODS_
-                    .setMethodId('')    // 여기에 API 키 입력
-                    .ajax(params1, params2, [function (data) {
-                    }, function (data) {
+                    .setMethodId('PDWorkReportSave')    // 여기에 API 키 입력
+                    .ajax(delArrData, [], [function (data) {
                         if (data[0].Status && data[0].Status != 0) {
                             // 뭔가 문제가 발생했을 때 리턴
                             alert('삭제 실패\n' + data[0].Result);
@@ -381,15 +375,11 @@ let app = new Vue({
                     }]);
                 }
             }
-
-            */
         },
 
         del: function(){
             let vThis = this;
-            alert('준비중입니다.');
 
-            /*
             if (confirm('모든 데이터를 삭제하시겠습니까?')) {
 
                 // 삭제 (외주발주에서 넘어온 데이터인 경우)
@@ -400,23 +390,13 @@ let app = new Vue({
                 // 삭제 (외주납품에서 넘어온 데이터인 경우)
                 else if (vThis.jumpSetMethodId == 'PDWorkReportJumpQuery') {
                     let delArrData = GX.deepCopy(vThis.rows.Query);
-                    let params1 = [], params2 = [];
-                    for (let i in delArrData) {
-                        let objParams = {};
-                        if (delArrData.hasOwnProperty(i)) {
-                            objParams.DelvSeq = delArrData[i].DelvSeq;      // ?
-                            objParams.DelvSerl = delArrData[i].DelvSerl;    // ?
-                            objParams.WorkingTag = 'D';
-                            params2.push(objParams);
-                        }
+                    for(let i in delArrData){
+                        delArrData[i].WorkingTag = 'D';
                     }
-
-                    params1.push(params2[0]);
 
                     GX._METHODS_
                     .setMethodId('PUDelvSave')
-                    .ajax(params1, params2, [function (data) {
-                    }, function (data) {
+                    .ajax(delArrData, [function (data) {
                         if (data[0].Status && data[0].Status != 0) {
                             // 뭔가 문제가 발생했을 때 리턴
                             alert('삭제 실패\n' + data[0].Result);
@@ -426,7 +406,6 @@ let app = new Vue({
                     }]);
                 }
             }
-            */
         }
     },
 
@@ -470,8 +449,8 @@ let app = new Vue({
                 .item('InWHName').head('입고창고', '').body(null, 'text-l')
                 .item('Remark').head('특이사항', '')
                     .body('<div><input type="text" name="Remark" attr-condition="" :value="row.Remark" @input="updateRowData(index)" style="border: 0px solid; text-align: left; background: transparent;" /></div>')
-                .item('IsEnd').head('완료여부', '') // 입력가능, 전체체크항목 있음.
-                    .body('<div><input type="checkbox" name="IsEnd" attr-condition="" :value="row.IsEnd" :checked="row.IsEnd" @input="updateRowData(index);" /></div>', '')
+                .item('IsEnd').head('완료여부<div class="chkBox"><input type="checkbox" @click="selectedAllIsEnd();" /></div>', '')
+                    .body('<div class="chkBox"><input type="checkbox" name="IsEnd" attr-condition="" :value="row.IsEnd" :checked="row.IsEnd" @input="selectedMarkIsEnd(index);" /></div>', '')
                 .item('OSPPrice').head('단가', '').body(null, 'text-r')
                 .item('IsVAT').head('부가세포함', '')
                     .body('<div class="chkBox"><input type="checkbox" name="IsVAT" :value="row.IsVAT" :checked="row.IsVAT" disabled/></div>')
