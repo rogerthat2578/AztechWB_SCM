cc7afadf25b65fd8442b06d7bb14965a53b2528e
lragusals <lragusals0@gmail.com>
2023-07-18 13:03:46 +0900

    외주납품품목조회  1) WorkCond8 : GbnSCM, WorkCond9 : IsCompDelvIn (from 배대일대리) 매핑을 위해 수정  2) SP 수정 : 조회조건 완료구분에 대한 처리가 안되어있어서 추가함 (100%)   - 구매납품품목조회의 SP와 파라메터 추출 방식이 달라 시간이 좀 더 소요됨  3) 테스트 : 조회조건에 따라 정상적으로 조회, 납품구분이 완료된 데이터 체크로 표기되는지 확인 (100%)

--- scm_front/js/outsourcing_purchase_delivery_search.js
+++ scm_front/js/outsourcing_purchase_delivery_search.js
@@ -22,8 +22,8 @@ let app = new Vue({
             ProcessName: '전체',
             Dept: 0,
             DeptName: '전체',
-            WorkCond8: '2',
-            WorkCond8Name: '전체',
+            IsCompDelvIn: '2',
+            IsCompDelvInName: '전체',
         },
         // 공정 리스트
         ProcessNameList: [],
@@ -34,13 +34,13 @@ let app = new Vue({
         // 부서 리스트
         KeepDeptNameList: [],
         // (납품)완료구분 리스트
-        WorkCond8NameList: [
+        IsCompDelvInNameList: [
             {key: 2, val: '전체'},
             {key: 0, val: '납품중'},
             {key: 1, val: '납품완료'}
         ],
         // (납품)완료구분 리스트
-        KeepWorkCond8NameList: [
+        KeepIsCompDelvInNameList: [
             {key: 2, val: '전체'},
             {key: 0, val: '납품중'},
             {key: 1, val: '납품완료'}
@@ -138,10 +138,10 @@ let app = new Vue({
                         vThis.queryForm.DeptName = vThis.KeepDeptNameList[0].val;
                     }
                     // (납품)완료구분 리스트 Select Box 초기화
-                    if ((vThis.WorkCond8NameList.length == 1 && (vThis.WorkCond8NameList[0].val == '전체' || vThis.WorkCond8NameList[0].val == '')) || vThis.queryForm.WorkCond8Name.replace(/\ /g, '') == '') {
-                        vThis.WorkCond8NameList = vThis.KeepWorkCond8NameList;
-                        vThis.queryForm.WorkCond8 = vThis.KeepWorkCond8NameList[0].key;
-                        vThis.queryForm.WorkCond8Name = vThis.KeepWorkCond8NameList[0].val;
+                    if ((vThis.IsCompDelvInNameList.length == 1 && (vThis.IsCompDelvInNameList[0].val == '전체' || vThis.IsCompDelvInNameList[0].val == '')) || vThis.queryForm.IsCompDelvInName.replace(/\ /g, '') == '') {
+                        vThis.IsCompDelvInNameList = vThis.KeepIsCompDelvInNameList;
+                        vThis.queryForm.IsCompDelvIn = vThis.KeepIsCompDelvInNameList[0].key;
+                        vThis.queryForm.IsCompDelvInName = vThis.KeepIsCompDelvInNameList[0].val;
                     }
                 }
             }
@@ -404,7 +404,7 @@ let app = new Vue({
         ToastUIGrid.setColumns
         .init()
         .setRowHeaders('rowNum')
-        .header('납품구분').name('WorkCond8').align('center').width(65).whiteSpace().ellipsis().formatter('checkbox', {attrDisabled: 'disabled', colKey: 'WorkCond8'}).sortable().setRow()
+        .header('납품구분').name('IsCompDelvIn').align('center').width(65).whiteSpace().ellipsis().formatter('checkbox', {attrDisabled: 'disabled', colKey: 'IsCompDelvIn'}).sortable().setRow()
         .header('작업일').name('WorkDate').align('center').width(100).whiteSpace().ellipsis().formatter('addHyphen8length').sortable(true).setRow()
         .header('품번').name('GoodItemNo').align('left').width(140).whiteSpace().ellipsis().sortable(true).setRow()
         .header('품명').name('GoodItemName').align('left').width(120).whiteSpace().ellipsis().sortable(true).setRow()
@@ -466,9 +466,9 @@ let app = new Vue({
                         vThis.objDblClick.click = false;
                         vThis.objDblClick.time = 0;
 
-                        const WorkCond8Value = GX._METHODS_.nvl(vThis.mainGrid.getValue(e.rowKey, 'WorkCond8'));
+                        const IsCompDelvInValue = GX._METHODS_.nvl(vThis.mainGrid.getValue(e.rowKey, 'IsCompDelvIn'));
 
-                        if (WorkCond8Value == 1) {
+                        if (IsCompDelvInValue == 1) {
                             toastr.warning('납품완료 상태는 수정, 삭제할 수 없습니다.');
                             return false;
                         }
